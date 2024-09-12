a71d0b8684c4d1957da7218b2d8704a62a537057
gerdasiitan <gerda.siitan13@gmail.com>
2024-09-12 11:15:51 +0300

    gdfgdg

--- learnphp2/src/Router.php
+++ learnphp2/src/Router.php
@@ -1,5 +1,19 @@
 <?php
 
 class Router {
+    private static $route = [];
+    private $path;
 
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    public function match() {
+        foreach(self::$routes as $route){
+            if($route['path'] === $this->path){
+                return $path
+            }
+        }
+    }
 }
