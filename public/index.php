a70923a35f0084356e7d1b2e0664454ec3d09ddf
Gerda Siitan <60605080235@tptlive.ee>
2024-09-27 12:45:25 +0300

    fix everything

--- public/index.php
+++ public/index.php
@@ -6,8 +6,10 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|svg|js|css)$/', $_SERVER["REQUEST_URI"]))
 
 spl_autoload_register(function($class){
     $class = substr($class, 4);
-    require_once "src/$class.php";
+    require_once __DIR__ . "/../src/$class.php";
 });
+
+session_start();
 
 require __DIR__ . '/../helpers.php';
 require __DIR__ . '/../routes.php';
