51c7fbafe9b47778a4fab2eb72db6d1f5ccf031d
gerdasiitan <gerda.siitan13@gmail.com>
2024-09-28 18:51:23 +0300

    fgdf

--- src/Controllers/PostsController.php
+++ src/Controllers/PostsController.php
@@ -51,6 +51,5 @@ class PostsController
         }
         view('posts/view');
     }
-
     
 }
--- views/Posts/view.php
+++ views/Posts/view.php
@@ -1,6 +1,29 @@
-<?php include __DIR__ . '/../partials/header.php' ?>
+<?php include __DIR__ . '/../partials/header.php'  ?>
 
-<main class="container">
+<main class="container mt-4">
+    <article class="border rounded p-4">
+        <h2>Post Details</h2>
+        
+        <div class="mb-3">
+         <strong>Title:</strong> <?= isset($post) ? $post->title : 'ID not found'; ?>
+        </div>
+        
+        <div class="mb-3">
+            <strong>Content:</strong> <?= isset($post) ? $post->body : 'ID not found'; ?>
+        </div>
+        
+        <div class="mb-3">
+            <strong>ID:</strong> <?= isset($post) ? $post->id : 'ID not found'; ?>
+        </div>
+        
+        <div class="mb-3">
+            <strong>Published at:</strong> <?= isset($post) ? $post->created_at : 'Date not found'; ?>
+        </div>
+
+        <div class="mb-3">
+            <a href="/admin/posts" class="btn btn-secondary">Back to Posts</a>
+        </div>
+    </article>
 </main>
 
-<?php include __DIR__ . '/../partials/footer.php' ?>
+<?php include __DIR__ . '/../partials/footer.php'  ?>
